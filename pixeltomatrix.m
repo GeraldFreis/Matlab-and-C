09d587c1b3e0495f2bfde9b3db4b71a602b8f653
Gerald <91832029+GeraldFreis@users.noreply.github.com>
2022-04-04 00:07:28 +0930

    Update pixeltomatrix.m

--- pixeltomatrix.m
+++ pixeltomatrix.m
@@ -5,23 +5,21 @@ function array_of_img = pixeltomatrix(pics)
     % initialising the image we want to read and producing the size of the
     % array we need
     img = imread(pics, 'png');
-%     pixe = [];
     [rows, columns] = size(img);
-    array_of_img = zeros(rows, columns);
+    array_of_img = zeros(rows, columns/3);
     
-    % iterating through each pixel of the image and adding its greyscale  colour to the
-    % matrix
-    % we are going to do this by creating a new page of dimension 257*257 
-    % for each iteration and append this to the array_of_img vector
-    for i = 1:rows
-        for j = 1:columns % j = 1: 3: 257
-            array_of_img(i, j) = img(i, j);
+    % iterating through each pixel of the image and if it is black adding it to the matrix, otherwise
+    % making making the element at (row, column) in the matrix white
+    for row = 1:rows
+        for column = 1:columns/3 
+            if(img(row, column) == 0)
+                array_of_img(row, column) = img(row, column);
+            else
+                array_of_img(row, column) = 255;
+            end
+            
         end
-
     end
-    
-    % ensuring that the program works
-    fprintf("Matrix compiled\n");
-    % returning the multi-dimensional array created
-%     convertingimage = array_of_img;
+    imshow(array_of_img);
+    fprintf("pixel to matrix compiled\n\n");
 end
