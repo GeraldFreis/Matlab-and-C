7bccb430106356d647f9b441df4aad9fc0766e3b
Gerald <91832029+GeraldFreis@users.noreply.github.com>
2022-04-06 11:51:44 +0930

    Update findingsevens.m

--- Week-5/findingsevens.m
+++ Week-5/findingsevens.m
@@ -2,7 +2,8 @@ function sevens = findingsevens(matrix)
     [rows, columns] = size(matrix);
     % logic for seven:
     % we start at the top left of the seven so we have to move to the end of the row
-        % for 72 font calibri this is 44 units right
+        % for 72 font calibri this is 44 units right.
+        % if every pixel we iterate over is black, we can continue onto the next stage
     % we then move down the diagonal with a row increment of 2 for every column increment
         % the diagonal is decreasing the columns while increasing the row
     % if this passes then its a seven
@@ -12,11 +13,11 @@ function sevens = findingsevens(matrix)
             if(matrix(row, column)==0)
                 test = false;  % sentinel variable
                 xcrement_var = 0;
-                stage = 1;  % this is going to be the variable that contains the sta
+                stage = 1;  % this is going to be the variable that contains the stage
                 % of the loop, i.e what vector to initialise
 
                 while ~test
-                    if(stage==1)
+                    if(stage==1) % iterating 44 pixels right (by increasing the columns)
                         if(matrix(row, column+xcrement_var)==0 && xcrement_var < 44)
                             xcrement_var = xcrement_var + 1;
                         elseif(xcrement_var == 44)
@@ -29,7 +30,8 @@ function sevens = findingsevens(matrix)
                             sevens = 0;
                             return;
                         end
-                    elseif(stage==2)
+                    elseif(stage==2) % iterating 68 rows down and 34 columns to the left, and if all of the pixels we iterate over are black
+                        % we have a seven
                         if(matrix(new_row + 2*xcrement_var, new_column - xcrement_var)==0 && xcrement_var < 34)
                             xcrement_var = xcrement_var + 1;
                         elseif(xcrement_var == 34)
