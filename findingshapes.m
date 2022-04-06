d864c593676263c6c7ad61ea2e18ba9b8e205c12
Gerald <91832029+GeraldFreis@users.noreply.github.com>
2022-04-06 09:36:49 +0930

    Update findingshapes.m

--- findingshapes.m
+++ findingshapes.m
@@ -1,7 +1,8 @@
 %% This function is going to take the matrix composed in pixeltomatrix and look for adjacent elements to find shapes
 
-% iteratively taking the value of all pixels less than 50 and capturing
-% that in a new array
+% iteratively taking the value of any pixel that is zero (black) and storing that in a new matrix of pixels, and any other value is white. 
+% This is to clean the image such that shapes are easier to find as we now have the constraint that any number must be in complete black font.
+
 function array_of_shapes = findingshapes(matrix)
     [rows, columns] = size(matrix);
     array_of_shapes = zeros(rows, columns/3);
