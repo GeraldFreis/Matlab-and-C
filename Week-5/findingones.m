a12e59de933b4f0992026d8446702883d57d6982
Gerald <91832029+GeraldFreis@users.noreply.github.com>
2022-04-10 00:12:36 +0930

    Update findingones.m

--- Week-5/findingones.m
+++ Week-5/findingones.m
@@ -1,6 +1,10 @@
 %% checking if the number is 1
 function  one = findingones(matrix)
+    % finding the rows and columns of the image / matrix of pixels
     [rows, columns] = size(matrix);
+    
+    % iterating over every pixel in the image and checking, via a conditional, whether the pixel being iterated
+    % over is black, and if so implementing our logic to test if it is a one.
     for row = 1:rows
         for column = 1:columns
             if(matrix(row, column)==0 && matrix(row, column+2)==0)
@@ -13,11 +17,13 @@ function  one = findingones(matrix)
                 % and move at a 45 degree angle left
 
                 % initialising the variables
-                test = false;
-                final_row = 0;
-                decrement_counter = 0;
+                test = false;  % sentinal variable to control our while loop
+                final_row = 0;  % initialising the end row that will be travelled to
+                decrement_counter = 0; % initialising the decrement variable that will be increased for every iteration of the loop
+                 % this variable will test if the xth value beneath the row is still black, and if so continuing to iterate down
+                 % if not we are at the bottom of the one and then can move proceed with our logic.
 
-                while ~test
+                while ~test % iterating to the bottom of the shape
                     if(matrix(row+decrement_counter, column)~=0)
                         final_row = row + decrement_counter-1;                        
                         test = true;
@@ -31,14 +37,17 @@ function  one = findingones(matrix)
                 % moving left and right
                 test = false;
                 decrement_counter = 0;
-                while ~test
-                    if(matrix(final_row, column-decrement_counter)~=0)
-                        if(matrix(final_row, column+decrement_counter)==0)
+                while ~test % moving left and incrementing the movement counter
+                    if(matrix(final_row, column-decrement_counter)~=0) % decreasing the columns so that we iterate leftwards and checking if the pixel is black
+                        % if the pixel is not black we want to take the distance we moved left, and if we can move that distance right and 
+                        % the pixel we land on when we move right is still black then we have passed this test.
+                        
+                        if(matrix(final_row, column+decrement_counter)==0) % moving the distance we moved left right and che
 %                             fprintf("A one was found\n");
 %                             one = 1;
                             test = true;
 %                             return;
-                        else
+                        else % if the pixel is not black (as the gradient is not zero)
                             fprintf("No ones\n");
                             one = 0;
                             test = true;
@@ -46,18 +55,18 @@ function  one = findingones(matrix)
                         end
 %                         test = true;
                     else
-                        decrement_counter = decrement_counter + 1;
+                        decrement_counter = decrement_counter + 1; % incrementing the movement as the pixel is still black
 %                         fprintf("%d\n", decrement_counter);
                     end
                 
                 end
 
                 % going back to the top and moving down the 45 degree diagonal
-                if(matrix(row+10, column-10)==0)
+                if(matrix(row+10, column-10)==0) % if the pixel is still black after we move 10 pixels down and left we have a one
                     one = 1;
                     fprintf("A one was found\n");
                     return;
-                else
+                else % if the pixel is not black we do not have a one
                     one = 0;
                     fprintf("Not a one\n");
                     return;
