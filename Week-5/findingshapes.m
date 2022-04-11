0b19e156fa183f36bbbc609639572d9acc25f24a
Gerald <91832029+GeraldFreis@users.noreply.github.com>
2022-04-11 11:31:38 +0930

    Update findingshapes.m

--- Week-5/findingshapes.m
+++ Week-5/findingshapes.m
@@ -4,20 +4,22 @@
 % producing a new matrix from this and returning that cleaned matrix.
 
 function array_of_shapes = findingshapes(matrix)
-    [rows, columns] = size(matrix);
-    array_of_shapes = zeros(rows, columns/3);
+    [rows, columns] = size(matrix); % finding the rows and columns of the initial matrix
+    array_of_shapes = zeros(rows, columns/3); % initialising the new cleaned matrix, however, the columns in the initial are triple of what is required
     row = 1;
+    
     while row <= rows
         for column = 1:columns/3
-            if(matrix(row, column) == 0)
+            if(matrix(row, column) == 0) % iterating over every pixel and checking if the pixel is black and if so storing it in the vector
                 array_of_shapes(row, column) = matrix(row, column);
-            else
+            else % if the pixel is not black storing a white pixel in the matrix, this will clean the image
                 array_of_shapes(row, column) = 255;
             end
             
         end
         row = row + 1;
     end
-    imshow(array_of_shapes);
+    
+    imshow(array_of_shapes); % displaying the new, cleaned image
     fprintf("Finding shapes compiled\n");
 end
