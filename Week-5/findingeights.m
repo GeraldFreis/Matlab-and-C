a4902ceb24ddac2653c4db76cb76d030a5eeb476
Gerald <91832029+GeraldFreis@users.noreply.github.com>
2022-04-11 13:24:26 +0930

    Update findingeights.m

--- Week-5/findingeights.m
+++ Week-5/findingeights.m
@@ -25,95 +25,114 @@ function eights = findingeights(matrix)
                 
                 % initialising variables
                 test = false;  % sentinel variable
-                xcrement_var = 0;
+                xcrement_var = 0; % variable to contain the increment factor which will increment over pixels by changing the x and y (column and row),
+                % this will emulate a vector as we are changing the x and y position of the terminal point of the vector in each iteration
+
                 stage = 1;  % this is going to be the variable that contains the sta
                 % of the loop, i.e what vector to initialise
                 column_left = column+5;
                 column_right = column+5;
                 new_row = row + 2;
 
-                while ~test
+                while ~test % the while loop that will enable us to iterate over every pixel
                     if(stage==1)
-                        if(matrix(new_row + xcrement_var, column_left - (2 * xcrement_var))==0 && matrix(new_row+xcrement_var, column_right + (2 * xcrement_var))==0 && xcrement_var < 10)
+                        if(matrix(new_row + xcrement_var, column_left - (2 * xcrement_var))==0 && ...
+                        matrix(new_row+xcrement_var, column_right + (2 * xcrement_var))==0 && ...
+                        xcrement_var < 10) % testing if the current row and column of each vector is on a black pixel and if the current vector length is less than the required length
+                            % if so we increment the xcrement var so that we can continue moving in the required direction
                             xcrement_var = xcrement_var + 1;
-                        elseif(xcrement_var == 10)
+                        elseif(xcrement_var == 10) % if every pixel iterated over was black and we have reached the required lenghtn of the vector
                             column_left = column_left - (2 * xcrement_var);
                             column_right = column_right + (2 * xcrement_var);
                             new_row = new_row + xcrement_var;
                             xcrement_var = 0;
                             stage = stage + 1;
-%                             fprintf("%d, %d\n", new_row, column_right);
-%                             fprintf("%d, %d\n", new_row, column_left);
-                        else
+                        else % if the current pixel is not black, hence we have not passed this vector test and we do not have an eight
                             fprintf("Not a Eight\n");
                             eights = 0;
                             return;
                         end
+                        
                     elseif(stage==2)
-                        if(matrix(new_row + xcrement_var, column_left)==0 && matrix(new_row + xcrement_var, column_right)==0 && xcrement_var < 9)
+                        if(matrix(new_row + xcrement_var, column_left)==0 && ...
+                        matrix(new_row + xcrement_var, column_right)==0 && ...
+                        xcrement_var < 9)% testing if the current row and column of each vector is on a black pixel and if the current vector length is less than the required length
+                            % if so we increment the xcrement var so that we can continue moving in the required direction
                             xcrement_var = xcrement_var + 1;
-                        elseif(xcrement_var == 9)
+                        elseif(xcrement_var == 9) % if every pixel iterated over was black and we have reached the required lenghtn of the vector
                             new_row = new_row + xcrement_var;
                             xcrement_var = 0;
                             stage = stage + 1;
 %                             fprintf("%d", new_row, column_left);
-                        else
+                        else % if the current pixel is not black, hence we have not passed this vector test and we do not have an eight
                             fprintf("Not a Eight\n");
                             
                             eights = 0;
                             return;
                         end
                     elseif(stage == 3)
-                        if(matrix(new_row+xcrement_var, column_left + xcrement_var)==0 && matrix(new_row + xcrement_var, column_right - xcrement_var)==0 && xcrement_var <  18)
+                        if(matrix(new_row+xcrement_var, column_left + xcrement_var)==0 && ...
+                        matrix(new_row + xcrement_var, column_right - xcrement_var)==0 && ...
+                        xcrement_var <  18) % testing if the current row and column of each vector is on a black pixel and if the current vector length is less than the required length
+                            % if so we increment the xcrement var so that we can continue moving in the required direction
                             xcrement_var = xcrement_var + 1;
-                        elseif(xcrement_var==18)
-                            new_row = new_row + xcrement_var-2;
+                        elseif(xcrement_var==18) % if all pixels iterated over were black and we have reached the required length of the vector
+                            new_row = new_row + xcrement_var-2;  % updating the row and column so that the terminal row and column of this vector are the initial row and columns of the next vector
                             column_left = column_left + xcrement_var;
                             column_right = column_right - xcrement_var;
                             xcrement_var = 0;
                             stage = stage + 1;
-                        else
+                        else % if the current pixel is not black, hence we have not passed this vector test and we do not have an eight
                             fprintf("Not a Eight\n");
                             eights = 0;
                             return;
                         end
                     elseif(stage == 4)
-                        if(matrix(new_row + xcrement_var, column_left - xcrement_var)==0 && matrix(new_row + xcrement_var, column_right + xcrement_var)==0 && xcrement_var < 21)
+                        if(matrix(new_row + xcrement_var, column_left - xcrement_var)==0 && ...
+                        matrix(new_row + xcrement_var, column_right + xcrement_var)==0 && ...
+                        xcrement_var < 21) % testing if the current row and column of each vector is on a black pixel and if the current vector length is less than the required length
+                            % if so we increment the xcrement var so that we can continue moving in the required direction
                             xcrement_var = xcrement_var + 1;
-                        elseif(xcrement_var==21)
-                            new_row = new_row + xcrement_var+1;
+                        elseif(xcrement_var==21) % if every pixel iterated over was black and we have reached the required lenghtn of the vector
+                            new_row = new_row + xcrement_var+1;  % updating the row and columns so that the terminal row and column of this vector are the initial row and columns of the next vector
                             column_left=  column_left - xcrement_var;
                             column_right = column_right + xcrement_var;
                             xcrement_var = 0;
-                            stage = stage + 1;
-                        else
+                            stage = stage + 1; % increasing to the next stage as we have passed this vector test
+                        else % if the current pixel is not black, hence we have not passed this vector test and we do not have an eight
                             fprintf("Not a Eight\n");
                             eights = 0;
                             return;
                         end
                     elseif(stage == 5)
-                        if(matrix(new_row + xcrement_var, column_left + xcrement_var)==0 && matrix(new_row + xcrement_var, column_right - xcrement_var)==0 && xcrement_var < 17)
+                        if(matrix(new_row + xcrement_var, column_left + xcrement_var)==0 && ...
+                        matrix(new_row + xcrement_var, column_right - xcrement_var)==0 && ...
+                        xcrement_var < 17) % testing if the current row and column of each vector is on a black pixel and if the current vector length is less than the required length
+                            % if so we increment the xcrement var so that we can continue moving in the required direction
                             xcrement_var = xcrement_var + 1;
-                        elseif(xcrement_var == 17)
-                            new_row = new_row + xcrement_var;
+                        elseif(xcrement_var == 17) % if every pixel iterated over was black and we have reached the required lenghtn of the vector
+                            new_row = new_row + xcrement_var;  % updating the row and columns so that the terminal row and column of this vector are the initial row and columns of the next vector
                             column_left = column_left + xcrement_var;
                             column_right = column_right - xcrement_var;
                             xcrement_var = 0;
-                            stage = stage + 1;
-                        else
+                            stage = stage + 1; % increasing to the next stage as we have passed this vector test
+                        else % if the current pixel is not black, hence we have not passed this vector test and we do not have an eight
                             fprintf("Not a Eight\n");
 %                             fprintf("%d, %d, %d", xcrement_var, column_left+xcrement_var, new_row+xcrement_var);
                             eights = 0;
                             return;
                         end
                     elseif(stage==6)
-                        if(matrix(new_row, column_left + xcrement_var)==0 && matrix(new_row, column_right - xcrement_var)==0 && xcrement_var < 6)
+                        if(matrix(new_row, column_left + xcrement_var)==0 && ...
+                        matrix(new_row, column_right - xcrement_var)==0 && ...
+                        xcrement_var < 6) % testing if the current row and column of each vector is on a black pixel and if the current vector length is less than the required length
+                            % if so we increment the xcrement var so that we can continue moving in the required direction
                             xcrement_var = xcrement_var + 1;
-                        elseif(xcrement_var == 6)
-                            column_left = column_left + xcrement_var;
+                        elseif(xcrement_var == 6) % if we have reached the length of the vector we have passed the test
+                            column_left = column_left + xcrement_var;  % updating the row and columns so that the terminal row and column of this vector are the initial row and columns of the next vector
                             column_right = column_right - xcrement_var;                            
-                            if(column_left == column_right)
-                                eights = 1;
+                            if(column_left == column_right) % if, when we move 6 inwards, the terminal point for the left and right vector are the same we have an eight
+                                eights = 1; 
                                 fprintf("An eight was found")
                                 return;
                             end
