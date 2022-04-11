642555fab5a19408aa0fb4e6a096dd259f9ed613
Gerald <91832029+GeraldFreis@users.noreply.github.com>
2022-04-11 13:52:33 +0930

    Update findingsevens.m

--- Week-5/findingsevens.m
+++ Week-5/findingsevens.m
@@ -1,17 +1,22 @@
 function sevens = findingsevens(matrix)
-    [rows, columns] = size(matrix);
+    [rows, columns] = size(matrix); % finding the rows and columns of the matrix so that we can iterate over every pixel in the matrix
+    
     % logic for seven:
     % we start at the top left of the seven so we have to move to the end of the row
         % for 72 font calibri this is 44 units right
     % we then move down the diagonal with a row increment of 2 for every column increment
         % the diagonal is decreasing the columns while increasing the row
     % if this passes then its a seven
-
+    
+    % iterating over every pixel in the matrix and if that pixel is black (0), applying our logic to test if the pixel is part of a seven
     for row = 1:rows
         for column = 1:columns
-            if(matrix(row, column)==0)
-                test = false;  % sentinel variable
-                xcrement_var = 0;
+            if(matrix(row, column)==0) % if the pixel is black applying our logic
+                
+                % initialising our variables
+                test = false;  % sentinel variable to control the while loop
+                xcrement_var = 0; % variable to contain the increment factor to increment over pixels by changing the x and y (column and row), as
+                % this will emulate a vector
                 stage = 1;  % this is going to be the variable that contains the sta
                 % of the loop, i.e what vector to initialise
 
@@ -20,12 +25,12 @@ function sevens = findingsevens(matrix)
                         % we can return this new column to the next stage, so that it can move from that position diagonally left
                         if(matrix(row, column+xcrement_var)==0 && xcrement_var < 44)
                             xcrement_var = xcrement_var + 1;
-                        elseif(xcrement_var == 44)
-                            new_column = column + xcrement_var;
+                        elseif(xcrement_var == 44) % if every pixel iterated over was black we have made it to the end of this vector and hence can move to the next stage / vector
+                            new_column = column + xcrement_var;  % updating the row and column so that the terminal row and column of this vector are the initial row and columns of the next vector
                             new_row = row + 10;
-                            xcrement_var = 0;
-                            stage = stage + 1;
-                        else
+                            xcrement_var = 0; % resetting the increment factor
+                            stage = stage + 1; % moving to the next stage
+                        else % if a pixel we iterate over is not black returning to the main file as the vector test has not been passed
                             fprintf("Not a seven\n");
                             sevens = 0;
                             return;
@@ -34,16 +39,17 @@ function sevens = findingsevens(matrix)
                         % we have a seven
                         if(matrix(new_row + 2*xcrement_var, new_column - xcrement_var)==0 && xcrement_var < 34)
                             xcrement_var = xcrement_var + 1;
-                        elseif(xcrement_var == 34)
+                        elseif(xcrement_var == 34) % if every pixel iterated over was black we have made it to the end of this vector and hence can close the while loop
                             test = true;
-                        else
+                        else % if a pixel we iterate over is not black returning to the main file as the vector test has not been passed
                             fprintf("Not a seven\n");
-                            fprintf("%d, %d, %d", new_row, new_column, xcrement_var);
                             sevens = 0;
                             return;
                         end
                     end
                 end
+                
+                % as we have passed the vector tests for a seven, we have a seven and can return this information to the calling function
                 sevens = 1;
                 fprintf("A seven was found\n");
                 return;
