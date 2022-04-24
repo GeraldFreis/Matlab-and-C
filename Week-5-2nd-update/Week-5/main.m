31dbca31b0d4d785582ae2a00fbc01e50c7a6ac4
Gerald <91832029+GeraldFreis@users.noreply.github.com>
2022-04-24 23:10:29 +0930

    Update main.m

--- Week-5-2nd-update/Week-5/main.m
+++ Week-5-2nd-update/Week-5/main.m
@@ -6,14 +6,15 @@ clear
 % tp.takingpic
 var = 'number_8.png';
 % var = 'number_1.png';
-pixelmatrix = pixeltomatrix(var);
-shaped_array = findingshapes(pixelmatrix);
-pluses = findingplussymbol(shaped_array);
-one = findingones(shaped_array);
-twos = findingtwos(shaped_array);
-threes = findingthrees(shaped_array);
-fours = findingfours(shaped_array);
-fives = findingfives(shaped_array);
-sixes = findingsixes(shaped_array);
-sevens = findingsevens(shaped_array);
-eights = findingeights(shaped_array);
+
+pixelmatrix = pixeltomatrix(var); % creating a matrix of the pixels in the image
+shaped_array = findingshapes(pixelmatrix); % cleaning the matrix of pixels 
+pluses = findingplussymbol(shaped_array); % testing if a plus is in the image
+one = findingones(shaped_array); % testing if a one is in the image
+twos = findingtwos(shaped_array); % testing if a two is in the image
+threes = findingthrees(shaped_array); % testing if a three is in the image
+fours = findingfours(shaped_array); % testing if a four is in the image
+fives = findingfives(shaped_array); % testing if a five is in the image
+sixes = findingsixes(shaped_array); % testing if a six is in the image
+sevens = findingsevens(shaped_array); % testing if a seven is in the image
+eights = findingeights(shaped_array); % testing if an eight is in the image
