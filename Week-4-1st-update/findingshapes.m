c86cd5142e94cdd1f3ce2f9a177053e52ee26d1f
Gerald <91832029+GeraldFreis@users.noreply.github.com>
2022-04-24 23:02:33 +0930

    Update findingshapes.m

--- Week-4-1st-update/findingshapes.m
+++ Week-4-1st-update/findingshapes.m
@@ -4,9 +4,11 @@
 % This is to clean the image such that shapes are easier to find as we now have the constraint that any number must be in complete black font.
 
 function array_of_shapes = findingshapes(matrix)
-    [rows, columns] = size(matrix);
-    array_of_shapes = zeros(rows, columns/3);
+    [rows, columns] = size(matrix); % finding the total rows and columns of the matrix
+    array_of_shapes = zeros(rows, columns/3); % initialising the matrix to store the cleaned pixels and accounting for an error where the image is repeated 3 times
     row = 1;
+    
+    % iterating over every pixel in the matrix and if its black transferring it to the clean array and if not making that pixel white
     while row <= rows
         for column = 1:columns/3
             if(matrix(row, column) == 0)
@@ -16,8 +18,9 @@ function array_of_shapes = findingshapes(matrix)
             end
             
         end
-        row = row + 1;
+        row = row + 1; % incrementing the row
     end
-    imshow(array_of_shapes);
+    
+    imshow(array_of_shapes); % showing the image
     fprintf("Finding shapes compiled\n");
 end
