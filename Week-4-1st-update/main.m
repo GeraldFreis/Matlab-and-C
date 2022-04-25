303dcdf5cb45c12d49712434337ab2dad692d9cc
Gerald <91832029+GeraldFreis@users.noreply.github.com>
2022-04-25 11:12:43 +0930

    Update main.m

--- Week-4-1st-update/main.m
+++ Week-4-1st-update/main.m
@@ -1,2 +1,2 @@
 matrix = pixeltomatrix(); % taking a picture and making a matrix from the pixels in the image
-cleanedmatrix = findingshapes() % cleaning the matrix
+cleanedmatrix = findingshapes(matrix) % cleaning the matrix
